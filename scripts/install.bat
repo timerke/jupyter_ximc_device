3e5622a825474bef6f071986ec4d8f7e22b518f6
timerke <timerke@mail.ru>
2022-12-26 17:37:07 +0300

    Changed scripts

--- scripts/install.bat
+++ scripts/install.bat
@@ -1,5 +1,6 @@
 cd ..
 if exist venv rd /s/q venv
-python -m venv venv
+set PYTHON=c:\python\python38\python
+%PYTHON% -m venv venv
 venv\Scripts\python -m pip install --upgrade pip
 venv\Scripts\python -m pip install -r requirements.txt
